76c9cbcfcfba99e69013a26f4d332fca391beeee
“cesarforall” <cesaralmeida89@gmail.com>
2022-09-22 00:28:28 +0200

    data ordered

--- script.js
+++ script.js
@@ -1,279 +1,533 @@
 comments = {
-	Desactivación: [
-		{
-			Desactivación: 'tamper',
-		},
-		{
-			Desactivación: 'tamper Time OV',
-		},
-		{
-			Desactivación: 'tamper Voltage',
-		},
-		{
-			Desactivación: 'tamper Temperatura',
-		},
-		{
-			Desactivación: 'tamper Pin 2-0',
-		},
-		{
-			Desactivación: 'tamper Pin 3-1',
-		},
-		{
-			Desactivación: 'tamper Pin 6-4',
-		},
-		{
-			Desactivación: 'tamper Pin 7-5',
-		},
-		{
-			Desactivación: 'tamper test mode',
-		},
-		{
-			Desactivación: 'tamper Monotonic',
-		},
-		{
-			Desactivación: 'tamper Clock',
-		},
-		{
-			Desactivación: 'tamper Security',
-		},
-		{
-			Desactivación: 'tamper Inicio-SD',
-		},
-		{
-			Desactivación: 'tamper KB',
-		},
-		{
-			Desactivación: 'tamper PCB',
-		},
-		{
-			Desactivación: 'tamper COR1',
-		},
-		{
-			Desactivación: 'tamper COR2',
-		},
-		{
-			Desactivación: 'tamper PIN0',
-		},
-		{
-			Desactivación: 'tamper PIN1',
-		},
-		{
-			Desactivación: 'tamper PIN2',
-		},
-		{
-			Desactivación: 'tamper PIN3',
-		},
-		{
-			Desactivación: 'tamper PIN4',
-		},
-		{
-			Desactivación: 'tamper PIN5',
-		},
-		{
-			Desactivación: 'tamper PIN6',
-		},
-		{
-			Desactivación: 'tamper PIN7',
-		},
-	],
-	Sustitución: [
-		{
-			Sustitución: 'Cargador',
-		},
-		{
-			Sustitución: 'Batería',
-		},
-		{
-			Sustitución: 'Placa base',
-		},
-		{
-			Sustitución: 'Placa subordinada',
-		},
-		{
-			Sustitución: 'SD Card',
-		},
-		{
-			Sustitución: 'Altavoz',
-		},
-		{
-			Sustitución: 'Microfono',
-		},
-		{
-			Sustitución: 'Panel táctil',
-		},
-		{
-			Sustitución: 'Display',
-		},
-		{
-			Sustitución: 'KIT Display + Carcasa superior',
-		},
-		{
-			Sustitución: 'Carcasa superior',
-		},
-		{
-			Sustitución: 'Carcasa trasera',
-		},
-		{
-			Sustitución: 'Carcasa intermedia',
-		},
-		{
-			Sustitución: 'Tapa bateria',
-		},
-		{
-			Sustitución: 'Tapa papel',
-		},
-		{
-			Sustitución: 'Impresora',
-		},
-		{
-			Sustitución: 'Rodillo',
-		},
-		{
-			Sustitución: 'Antena',
-		},
-		{
-			Sustitución: 'Antena GPS',
-		},
-		{
-			Sustitución: 'Antena GSM',
-		},
-		{
-			Sustitución: 'Antena WIFI',
-		},
-		{
-			Sustitución: 'Antena Bluetooth',
-		},
-		{
-			Sustitución: 'Antena contactless',
-		},
-		{
-			Sustitución: 'Vibrador',
-		},
-		{
-			Sustitución: 'Teclado',
-		},
-		{
-			Sustitución: 'Resistencias',
-		},
-		{
-			Sustitución: 'Condensador',
-		},
-		{
-			Sustitución: 'Conector de carga',
-		},
-		{
-			Sustitución: 'Conector de bateria',
-		},
-		{
-			Sustitución: 'Conector microUSB',
-		},
-		{
-			Sustitución: 'Conector USB tipo C',
-		},
-		{
-			Sustitución: 'Conector display',
-		},
-		{
-			Sustitución: 'Conector impresora',
-		},
-		{
-			Sustitución: 'Conector',
-		},
-		{
-			Sustitución: 'Buzzer',
-		},
-		{
-			Sustitución: 'Cámara frontal',
-		},
-		{
-			Sustitución: 'Cámara trasera',
-		},
-		{
-			Sustitución: 'Flex teclado',
-		},
-		{
-			Sustitución: 'Flex encendido',
-		},
-		{
-			Sustitución: 'Flex NFC',
-		},
-		{
-			Sustitución: 'Lector banda magnética',
-		},
-		{
-			Sustitución: 'Lector chip card',
-		},
-		{
-			Sustitución: 'Zocalo SIM',
-		},
-		{
-			Sustitución: 'Alimentador',
-		},
-		{
-			Sustitución: 'Láser',
-		},
-		{
-			Sustitución: 'lente escaner',
-		},
-		{
-			Sustitución: 'lente cámara trasera',
-		},
-		{
-			Sustitución: 'Pila',
-		},
-		{
-			Sustitución: 'Batería interna',
-		},
-		{
-			Sustitución: 'MicroSD',
-		},
-		{
-			Sustitución: 'Modem',
-		},
-		{
-			Sustitución: 'Placa teclado',
-		},
-		{
-			Sustitución: 'Flex Core',
-		},
-		{
-			Sustitución: 'Neoprenos',
-		},
-		{
-			Sustitución: 'Escudo',
-		},
-		{
-			Sustitución: 'Metal DOME',
-		},
-		{
-			Sustitución: 'Zebra (Tamper)',
-		},
-		{
-			Sustitución: 'PMIC',
-		},
-		{
-			Sustitución: 'Circuito integrado',
-		},
-		{
-			Sustitución: 'CPU',
-		},
-		{
-			Sustitución: 'Criptoprocesador',
-		},
-		{
-			Sustitución: 'Patas de goma',
-		},
-		{
-			Sustitución: 'Muro seguridad',
-		},
-		{
-			Sustitución: 'Pulsador de goma',
-		},
-		{
-			Sustitución: 'Pulsador',
-		},
-	],
-};
+    "Desactivación": [
+        {
+            "Desactivación": "tamper"
+        },
+        {
+            "Desactivación": "tamper Clock"
+        },
+        {
+            "Desactivación": "tamper COR1"
+        },
+        {
+            "Desactivación": "tamper COR2"
+        },
+        {
+            "Desactivación": "tamper Inicio-SD"
+        },
+        {
+            "Desactivación": "tamper KB"
+        },
+        {
+            "Desactivación": "tamper Monotonic"
+        },
+        {
+            "Desactivación": "tamper PCB"
+        },
+        {
+            "Desactivación": "tamper Pin 2-0"
+        },
+        {
+            "Desactivación": "tamper Pin 3-1"
+        },
+        {
+            "Desactivación": "tamper Pin 6-4"
+        },
+        {
+            "Desactivación": "tamper Pin 7-5"
+        },
+        {
+            "Desactivación": "tamper PIN0"
+        },
+        {
+            "Desactivación": "tamper PIN1"
+        },
+        {
+            "Desactivación": "tamper PIN2"
+        },
+        {
+            "Desactivación": "tamper PIN3"
+        },
+        {
+            "Desactivación": "tamper PIN4"
+        },
+        {
+            "Desactivación": "tamper PIN5"
+        },
+        {
+            "Desactivación": "tamper PIN6"
+        },
+        {
+            "Desactivación": "tamper PIN7"
+        },
+        {
+            "Desactivación": "tamper Security"
+        },
+        {
+            "Desactivación": "tamper Temperatura"
+        },
+        {
+            "Desactivación": "tamper test mode"
+        },
+        {
+            "Desactivación": "tamper Time OV"
+        },
+        {
+            "Desactivación": "tamper Voltage"
+        }
+    ],
+    "Sustitución": [
+        {
+            "Sustitución": "Alimentador"
+        },
+        {
+            "Sustitución": "Altavoz"
+        },
+        {
+            "Sustitución": "Antena"
+        },
+        {
+            "Sustitución": "Antena Bluetooth"
+        },
+        {
+            "Sustitución": "Antena contactless"
+        },
+        {
+            "Sustitución": "Antena GPS"
+        },
+        {
+            "Sustitución": "Antena GSM"
+        },
+        {
+            "Sustitución": "Antena WIFI"
+        },
+        {
+            "Sustitución": "Batería"
+        },
+        {
+            "Sustitución": "Batería interna"
+        },
+        {
+            "Sustitución": "Buzzer"
+        },
+        {
+            "Sustitución": "Cámara frontal"
+        },
+        {
+            "Sustitución": "Cámara trasera"
+        },
+        {
+            "Sustitución": "Carcasa intermedia"
+        },
+        {
+            "Sustitución": "Carcasa superior"
+        },
+        {
+            "Sustitución": "Carcasa trasera"
+        },
+        {
+            "Sustitución": "Cargador"
+        },
+        {
+            "Sustitución": "Circuito integrado"
+        },
+        {
+            "Sustitución": "Condensador"
+        },
+        {
+            "Sustitución": "Conector"
+        },
+        {
+            "Sustitución": "Conector de bateria"
+        },
+        {
+            "Sustitución": "Conector de carga"
+        },
+        {
+            "Sustitución": "Conector display"
+        },
+        {
+            "Sustitución": "Conector impresora"
+        },
+        {
+            "Sustitución": "Conector microUSB"
+        },
+        {
+            "Sustitución": "Conector USB tipo C"
+        },
+        {
+            "Sustitución": "CPU"
+        },
+        {
+            "Sustitución": "Criptoprocesador"
+        },
+        {
+            "Sustitución": "Display"
+        },
+        {
+            "Sustitución": "Escudo"
+        },
+        {
+            "Sustitución": "Flex Core"
+        },
+        {
+            "Sustitución": "Flex encendido"
+        },
+        {
+            "Sustitución": "Flex NFC"
+        },
+        {
+            "Sustitución": "Flex teclado"
+        },
+        {
+            "Sustitución": "Impresora"
+        },
+        {
+            "Sustitución": "KIT Display + Carcasa superior"
+        },
+        {
+            "Sustitución": "Láser"
+        },
+        {
+            "Sustitución": "Lector banda magnética"
+        },
+        {
+            "Sustitución": "Lector chip card"
+        },
+        {
+            "Sustitución": "lente cámara trasera"
+        },
+        {
+            "Sustitución": "lente escaner"
+        },
+        {
+            "Sustitución": "Metal DOME"
+        },
+        {
+            "Sustitución": "Microfono"
+        },
+        {
+            "Sustitución": "MicroSD"
+        },
+        {
+            "Sustitución": "Modem"
+        },
+        {
+            "Sustitución": "Muro seguridad"
+        },
+        {
+            "Sustitución": "Neoprenos"
+        },
+        {
+            "Sustitución": "Panel táctil"
+        },
+        {
+            "Sustitución": "Patas de goma"
+        },
+        {
+            "Sustitución": "Pila"
+        },
+        {
+            "Sustitución": "Placa base"
+        },
+        {
+            "Sustitución": "Placa subordinada"
+        },
+        {
+            "Sustitución": "Placa teclado"
+        },
+        {
+            "Sustitución": "PMIC"
+        },
+        {
+            "Sustitución": "Pulsador"
+        },
+        {
+            "Sustitución": "Pulsador de goma"
+        },
+        {
+            "Sustitución": "Resistencias"
+        },
+        {
+            "Sustitución": "Rodillo"
+        },
+        {
+            "Sustitución": "SD Card"
+        },
+        {
+            "Sustitución": "Tapa bateria"
+        },
+        {
+            "Sustitución": "Tapa papel"
+        },
+        {
+            "Sustitución": "Teclado"
+        },
+        {
+            "Sustitución": "Vibrador"
+        },
+        {
+            "Sustitución": "Zebra (Tamper)"
+        },
+        {
+            "Sustitución": "Zocalo SIM"
+        }
+    ],
+    "Reajuste": [
+        {
+            "Reajuste": "Conector"
+        },
+        {
+            "Reajuste": "Lector banda"
+        },
+        {
+            "Reajuste": "Lector chip card"
+        },
+        {
+            "Reajuste": "Lector MicroSD"
+        },
+        {
+            "Reajuste": "Lector SIM"
+        }
+    ],
+    "Resoldadura": [
+        {
+            "Resoldadura": "Circuito integrado"
+        },
+        {
+            "Resoldadura": "Conector"
+        },
+        {
+            "Resoldadura": "Conector de carga"
+        },
+        {
+            "Resoldadura": "CPU"
+        },
+        {
+            "Resoldadura": "Criptoprocesador"
+        },
+        {
+            "Resoldadura": "muro seguridad"
+        },
+        {
+            "Resoldadura": "PMIC"
+        },
+        {
+            "Resoldadura": "Pulsador"
+        }
+    ],
+    "Scrap": [
+        {
+            "Scrap": "Alimentador"
+        },
+        {
+            "Scrap": "Altavoz"
+        },
+        {
+            "Scrap": "Antena"
+        },
+        {
+            "Scrap": "Antena Bluetooth"
+        },
+        {
+            "Scrap": "Antena contactless"
+        },
+        {
+            "Scrap": "Antena GPS"
+        },
+        {
+            "Scrap": "Antena GSM"
+        },
+        {
+            "Scrap": "Antena WIFI"
+        },
+        {
+            "Scrap": "Batería"
+        },
+        {
+            "Scrap": "Batería interna"
+        },
+        {
+            "Scrap": "Buzzer"
+        },
+        {
+            "Scrap": "Cámara frontal"
+        },
+        {
+            "Scrap": "Cámara trasera"
+        },
+        {
+            "Scrap": "Carcasa intermedia"
+        },
+        {
+            "Scrap": "Carcasa superior"
+        },
+        {
+            "Scrap": "Carcasa trasera"
+        },
+        {
+            "Scrap": "Cargador"
+        },
+        {
+            "Scrap": "Circuito integrado"
+        },
+        {
+            "Scrap": "Condensador"
+        },
+        {
+            "Scrap": "Conector"
+        },
+        {
+            "Scrap": "Conector de bateria"
+        },
+        {
+            "Scrap": "Conector de carga"
+        },
+        {
+            "Scrap": "Conector display"
+        },
+        {
+            "Scrap": "Conector impreso"
+        },
+        {
+            "Scrap": "Conector microUS"
+        },
+        {
+            "Scrap": "Conector USB tipo C"
+        },
+        {
+            "Scrap": "CPU"
+        },
+        {
+            "Scrap": "Criptoprocesador"
+        },
+        {
+            "Scrap": "Display"
+        },
+        {
+            "Scrap": "Escudo"
+        },
+        {
+            "Scrap": "Flex Core"
+        },
+        {
+            "Scrap": "Flex encendido"
+        },
+        {
+            "Scrap": "Flex NFC"
+        },
+        {
+            "Scrap": "Flex teclado"
+        },
+        {
+            "Scrap": "Impresora"
+        },
+        {
+            "Scrap": "KIT Display + Carcasa superior"
+        },
+        {
+            "Scrap": "Láser"
+        },
+        {
+            "Scrap": "Lector banda magnética"
+        },
+        {
+            "Scrap": "Lector chip card"
+        },
+        {
+            "Scrap": "lente cámara trasera"
+        },
+        {
+            "Scrap": "lente escaner"
+        },
+        {
+            "Scrap": "Metal DOME"
+        },
+        {
+            "Scrap": "Microfono"
+        },
+        {
+            "Scrap": "MicroSD"
+        },
+        {
+            "Scrap": "Modem"
+        },
+        {
+            "Scrap": "Muro seguridad"
+        },
+        {
+            "Scrap": "Neoprenos"
+        },
+        {
+            "Scrap": "Panel táctil"
+        },
+        {
+            "Scrap": "Pila"
+        },
+        {
+            "Scrap": "Placa base"
+        },
+        {
+            "Scrap": "Placa subordinada"
+        },
+        {
+            "Scrap": "Placa teclado"
+        },
+        {
+            "Scrap": "PMIC"
+        },
+        {
+            "Scrap": "Resistencias"
+        },
+        {
+            "Scrap": "Rodillo"
+        },
+        {
+            "Scrap": "SD Card"
+        },
+        {
+            "Scrap": "Tapa bateria"
+        },
+        {
+            "Scrap": "Tapa papel"
+        },
+        {
+            "Scrap": "Teclado"
+        },
+        {
+            "Scrap": "Vibrador"
+        },
+        {
+            "Scrap": "Zebra (Tamper)"
+        },
+        {
+            "Scrap": "Zocalo SIM"
+        }
+    ],
+    "Varios": [
+        {
+            "Varios": "Actualización versión"
+        },
+        {
+            "Varios": "Carga de claves"
+        },
+        {
+            "Varios": "Devolución de terminal"
+        },
+        {
+            "Varios": "Devuelto irreparable"
+        },
+        {
+            "Varios": "Limpieza"
+        },
+        {
+            "Varios": "Plataformado"
+        },
+        {
+            "Varios": "Revisión tensión batería"
+        }
+    ]
+}
 const data = Object.entries(comments);
 const commentsContainer = document.getElementById('comments-container');
 const wholeCommentContainer = document.getElementById('whole-comment-container');
